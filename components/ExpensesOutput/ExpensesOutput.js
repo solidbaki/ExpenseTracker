b7c754fd78a18ec685b1991ea25d36583fbff215
Berke Baklan <berkebaklan@hotmail.com>
2022-08-20 13:34:24 +0300

    Expenses components, lists, dummy data added

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,47 @@
+import { View, Text, FlatList } from "react-native";
+import ExpensesList from "./ExpensesList";
+import ExpensesSummary from "./ExpensesSummary";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 99.99,
+    date: new Date("2022-12-30"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 9.99,
+    date: new Date("2022-05-15"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 8.99,
+    date: new Date("2022-06-08"),
+  },
+  {
+    id: "e5",
+    description: "Whiskas",
+    amount: 12.99,
+    date: new Date("2022-08-01"),
+  },
+];
+
+const ExpensesOutput = ({ expenses, expensesPeriod }) => {
+  return (
+    <View>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES}/>
+    </View>
+  );
+};
+
+export default ExpensesOutput;
