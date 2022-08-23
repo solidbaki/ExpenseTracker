fb5fa0063d48dff2eebd66553b43be57bc52e46e
Berke Baklan <berkebaklan@hotmail.com>
2022-08-23 23:40:44 +0300

    Input modals,  modifications on expense components

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,33 +1,53 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import { GlobalStyles } from "../../constants/styles";
-import { getFormattedDate } from "../../util/Date";
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const ExpenseItem = ({ description, amount, date }) => {
+import { GlobalStyles } from '../../constants/styles';
+import { getFormattedDate } from '../../util/date';
+
+function ExpenseItem({ id, description, amount, date }) {
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate('ManageExpense', {
+      expenseId: id
+    });
+  }
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
-        <Text style={[styles.textBase, styles.description]}>{description}</Text>
-        <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
-      </View>
-      <View style={styles.amountContainer}>
-        <Text style={styles.amount}>{amount.toFixed(2)}</Text>
+        <View>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
+        </View>
       </View>
     </Pressable>
   );
-};
+}
 
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
     backgroundColor: GlobalStyles.colors.primary500,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     borderRadius: 6,
-    elevation: 3, //For android
-    shadowColor: GlobalStyles.colors.gray500, //For ios
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.gray500,
     shadowRadius: 4,
     shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.4,
@@ -38,19 +58,19 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 16,
     marginBottom: 4,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   amountContainer: {
     paddingHorizontal: 12,
     paddingVertical: 4,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: 4,
     minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
 });
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
-import { getDateMinusDays } from '../util/Date';
+import { getDateMinusDays } from '../util/date';
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
